ca7660ac8b879574968f0c3808a47f0763aad683
prthm20 <aprathamm@gmail.com>
2024-07-12 04:41:33 +0530

    mobile optimized

--- src/app/Countryinfo/page.tsx
+++ src/app/Countryinfo/page.tsx
@@ -27,23 +27,22 @@ function Page() {
 
     return (
         <div className='min-h-screen bg-indigo-100'>
-  <Navbar />
-  <div className='flex flex-col items-center justify-center p-10'>
-    <div className='w-full max-w-md p-10 mb-10 mt-10 bg-white rounded-lg shadow-lg'>
-      <ComboboxDemo onSelectCountry={handleSelectCountry} />
-    </div>
-    <div className='mt-10 pt-2'>
-      <button
-        type="submit"
-        className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 p-5 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
-        onClick={handleSearchSubmit}
-      >
-        Search
-      </button>
-    </div>
-  </div>
-</div>
-
+            <Navbar />
+            <div className='flex flex-col items-center justify-center p-4 sm:p-10'>
+                <div className='w-full max-w-xs sm:max-w-md p-4 sm:p-10 mb-5 sm:mb-10 mt-5 sm:mt-10 bg-white rounded-lg shadow-lg'>
+                    <ComboboxDemo onSelectCountry={handleSelectCountry} />
+                </div>
+                <div className='mt-5 sm:mt-10'>
+                    <button
+                        type="submit"
+                        className="text-center mt-6 text-white font-extrabold bg-fuchsia-500 py-3 px-6 sm:py-5 sm:px-10 rounded-full border-2 border-fuchsia-500 shadow-lg transform transition duration-300 ease-in-out hover:bg-violet-300 hover:text-black hover:scale-105 hover:border-violet-300"
+                        onClick={handleSearchSubmit}
+                    >
+                        Search
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
