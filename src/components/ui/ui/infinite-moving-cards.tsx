c98910843c5edbb9fefa62ca5ce87b3bff5b8cf6
prthm20 <aprathamm@gmail.com>
2024-07-12 04:08:09 +0530

    mo

--- src/components/ui/ui/infinite-moving-cards.tsx
+++ src/components/ui/ui/infinite-moving-cards.tsx
@@ -15,8 +15,8 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
-    sourc: string;
-    query: string;
+    sourc:string;
+    query:string
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -76,24 +76,26 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20   max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
+        
       <ul
         ref={scrollerRef}
         className={cn(
-          "flex min-w-full shrink-0 gap-5 py-4 w-max flex-nowrap",
-          start && "animate-scroll",
+          " flex min-w-full shrink-0 gap-5 py-4 w-max flex-nowrap",
+          start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
+             
         {items.map((item, idx) => (
           <li
-            className="w-[80%] md:w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-100 px-4 md:px-8 py-6"
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-100 px-8 py-6 md:w-[450px]"
             style={{
               background:
-                "linear-gradient(180deg, var(--slate-100), var(--slate-100))",
+                "linear-gradient(180deg, var(--slate-100), var(--slate-100)",
             }}
             key={item.name}
           >
@@ -102,30 +104,26 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className="relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
                 {item.quote}
               </span>
               <Link href={`/countries/?query=${encodeURIComponent(item.query)}`}>
-                <div className="relative z-20 mt-6 flex flex-col items-center">
-                  <span className="flex flex-col gap-1">
-                    <span className="text-sm leading-[1.6] text-gray-400 font-normal">
-                      {item.name}
-                    </span>
-                    <span className="text-sm leading-[1.6] text-gray-400 font-normal">
-                      {item.title}
-                    </span>
-                    <span className="text-sm leading-[1.6] text-gray-400 font-normal w-full">
-                      <img
-                        src={item.sourc}
-                        alt={item.title}
-                        height="1000"
-                        width="1000"
-                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      />
-                    </span>
+              <div className="relative z-20 mt-6 flex flex-row items-center">
+                <span className="flex flex-col gap-1">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.name}
                   </span>
-                </div>
-              </Link>
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.title}
+                  </span>
+                  
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal w-full">
+                   <img src={item.sourc} alt=""  height="1000"  width="1000" className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"   /> 
+                  </span>
+
+                </span>
+              </div>
+        </Link>
             </blockquote>
           </li>
         ))}
