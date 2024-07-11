80e9a402a958131a505f7da2ecf7cf0033f76c3b
prthm20 <aprathamm@gmail.com>
2024-07-12 03:53:56 +0530

    mobile optimization

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,66 +1,61 @@
-
 import React from 'react'
 import Navbar from '../../components/ui/Navbar'
-
-
 import { InfiniteMovingCardsDemo } from '../../components/ui/Testimonials'
 import Foter from '../../components/ui/Foter'
 import { ThreeDCardDemo } from '../../components/ui/Category'
-
 import { connect } from '../../dbconfig/dbconfig'
 import Image from 'next/image'
 import Link from 'next/link'
 
-
-
 async function page() {
- 
   return (
     <div>
-    
-     <Navbar></Navbar>
-     <div className='p-28' >
-   
-<Image src="https://images.pexels.com/photos/3030268/pexels-photo-3030268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""  className=' rounded-2xl' width={1500} height={1500}/>
-     </div>
-     <div className=' text-center  text-2xl text-bold font-sans opacity-70'><h1>Categories</h1></div>
-    <div className='flex  '>
-  
-      <div><ThreeDCardDemo  title="Religious" sourc='https://images.pexels.com/photos/415708/pexels-photo-415708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' query='all religion all over world' ></ThreeDCardDemo></div> 
-       <ThreeDCardDemo title="Tourist" sourc='https://images.pexels.com/photos/161183/thailand-monks-temple-tourism-161183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' query='Top tourist places'></ThreeDCardDemo>
-       <ThreeDCardDemo title="Nature" sourc='https://images.pexels.com/photos/235731/pexels-photo-235731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' query='Nature'></ThreeDCardDemo>
-       <ThreeDCardDemo title="Wonders of world" sourc='https://images.pexels.com/photos/10980106/pexels-photo-10980106.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' query='wonders of world'></ThreeDCardDemo>
-      </div> 
-    
-<div className='p-5'>
-     <div >
-     <div className=' text-center  text-2xl text-bold font-sans opacity-70'><h1>Popular Destinations</h1></div>
-     </div>
-      <InfiniteMovingCardsDemo>
-
-      </InfiniteMovingCardsDemo>
+      <Navbar />
+      <div className='p-4 sm:p-8 md:p-16 lg:p-28'>
+        <Image 
+          src="https://images.pexels.com/photos/3030268/pexels-photo-3030268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+          alt="Beautiful landscape" 
+          className='rounded-2xl' 
+          width={1500} 
+          height={1500} 
+        />
+      </div>
+      <div className='text-center text-2xl font-bold font-sans opacity-70'>
+        <h1>Categories</h1>
+      </div>
+      <div className='flex flex-wrap justify-center gap-3 p-2'>
+        <ThreeDCardDemo 
+          title="Religious" 
+          sourc='https://images.pexels.com/photos/415708/pexels-photo-415708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+          query='all religion all over world' 
+        />
+        <ThreeDCardDemo 
+          title="Tourist" 
+          sourc='https://images.pexels.com/photos/161183/thailand-monks-temple-tourism-161183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+          query='Top tourist places' 
+        />
+        <ThreeDCardDemo 
+          title="Nature" 
+          sourc='https://images.pexels.com/photos/235731/pexels-photo-235731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+          query='Nature' 
+        />
+        <ThreeDCardDemo 
+          title="Wonders of world" 
+          sourc='https://images.pexels.com/photos/10980106/pexels-photo-10980106.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+          query='wonders of world' 
+        />
+      </div>
+      <div className='p-5'>
+        <div className='text-center text-2xl font-bold font-sans opacity-70'>
+          <h1>Popular Destinations</h1>
+        </div>
+        <InfiniteMovingCardsDemo />
+      </div>
+      <div>
+        <Foter />
+      </div>
     </div>
-    
-<div>
-  
-
-  
-  
-</div>
-    
-    
-   
-  <div>
-    <Foter></Foter>
-  </div>
-
-   
-   
-    </div>
-     
   )
 }
-
-
 
 export default page
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,38 +1,45 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar"
 import Link from 'next/link'
+
 function Navbar() {
   return (
     <div>
-         <nav>
-        <ul className='flex px-40 py-5   text-lg font-bold'>
-            <div className=' py-5 px-9'>
+      <nav className="bg-white shadow">
+        <ul className='flex flex-wrap justify-between items-center px-4 md:px-20 py-5 text-lg font-bold'>
+          <div className='py-2 md:py-5'>
             <Avatar>
-  <AvatarImage src="file:///C:/Users/pratham%20angdalwar/Downloads/travel-high-resolution-logo.svg" />
-  <AvatarFallback>Travlog</AvatarFallback>
-</Avatar>
-            </div  >
-            <div className=' flex px-60 text-slate-600 text-opacity-50'>
-            <li className='py-5 px-5 hover:text-black'> <Link href={'/Home'}>Home</Link></li>
-            <li className='py-5 px-5  hover:text-black'><Link href={'/Countryinfo'}>Countires</Link></li>
-            <li className='py-5 px-5  hover:text-black'> <Link href={`/topdest?query=${encodeURIComponent("World")}`}>Destiantions</Link></li>
-            <li className='py-5 px-5  hover:text-black'> <Link href={'/RoutePlan'}>PlanRoute</Link></li>
-            <li className='py-5 px-5  hover:text-black'> <Link href={'/contact'}>Contact</Link></li>
-            <li className='py-5 px-5  hover:text-black'><Link href={'/about'}>About</Link></li>
-
-            <li className='py-5  px-10'>  <Link  className="px-6 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold" href={'/sign-up'}>Signup</Link></li>
-          
-            </div>
+              <AvatarImage src="/path/to/your/image/travel-high-resolution-logo.svg" />
+              <AvatarFallback>Travlog</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className='flex flex-col md:flex-row text-slate-600 text-opacity-50'>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={'/Home'}>Home</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={'/Countryinfo'}>Countries</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={`/topdest?query=${encodeURIComponent("World")}`}>Destinations</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={'/RoutePlan'}>Plan Route</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={'/contact'}>Contact</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-5 hover:text-black'>
+              <Link href={'/about'}>About</Link>
+            </li>
+            <li className='py-2 md:py-5 px-3 md:px-10'>
+              <Link className="px-6 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold" href={'/sign-up'}>
+                Signup
+              </Link>
+            </li>
+          </div>
         </ul>
-        <span>
-
-            <div>
-
-           
-            </div>
-        </span>
       </nav>
-      
     </div>
   )
 }
